638cd8415c33a955d91093d1a3503ff9436bf176
wllover <1105322177@qq.com>
2017-11-21 00:31:59 +0800

    git_test20171120

---
+++ src/cn/com/wllover/HelloWorld.java
@@ -0,0 +1,7 @@
+package cn.com.wllover;
+
+public class HelloWorld {
+	public static void main(String arg[]) {
+		System.out.println("Hello,My job!Today is 2017-11-20,I will work hard ...");
+	}
+}
---
+++ src/cn/com/wllover/helloWorldTest.java
@@ -0,0 +1,16 @@
+package cn.com.wllover;
+
+import org.junit.jupiter.api.Test;
+
+public class helloWorldTest {
+
+	@Test
+	public void StringTest() {
+		String a = "123";
+		String b = "456";
+		
+		String c = a+b;
+		System.out.println(c);
+	}
+	
+}
